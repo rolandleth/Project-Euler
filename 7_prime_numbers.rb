218e85cf9686deddf2884aea6c628e37197c41af
Roland Leth <roland@Rolands-iMac.local>
2013-08-30 16:43:59 +0300

    Added new problems.

--- 7_prime_numbers.rb
+++ 7_prime_numbers.rb
@@ -1,19 +1,27 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+
 primes = [2, 3]
+
+class Integer < Numeric
+  def is_prime?
+    return false if self <= 1
+    2.upto(Math.sqrt(self).to_i) do |x|
+      return false if self % x == 0
+    end
+    true
+  end
+end
+
 while primes.count < 10_001 do
   checked_number = primes.last
   is_prime = false
   until is_prime
     checked_number += 1
-    half = (Math.sqrt(checked_number) + 1).to_i
-    (2..half).each do |x|
-      if checked_number % x == 0
-        is_prime = false
-        break
-      else
-        is_prime = true
-      end
+    if checked_number.is_prime?
+      primes << checked_number
+      is_prime = true
     end
-    primes << checked_number if is_prime
   end
 end
 puts primes.last
